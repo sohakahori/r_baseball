0549d6619d3117cf1514cc1e303c304e97f57748
sohakahori <uu138994@RLSUU138994M01.local>
2018-10-25 20:38:07 +0900

    rspec環境構築

--- config/application.rb
+++ config/application.rb
@@ -19,5 +19,13 @@ module Baseball
     config.active_record.default_timezone = :local
     config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '**', '*.{rb,yml}').to_s]
     config.i18n.default_locale = :ja
+
+    config.generators do |g|
+      g.test_framework :rspec,
+      fixtures: false,
+      view_specs: false,
+      helper_specs: false,
+      routing_specs: false
+    end
   end
 end
