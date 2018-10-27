37b4e590c67545f88b9296ee2815f26b54563aed
sohakahori <sohakahori@gmail.com>
2018-10-27 11:04:24 +0900

    factory_bot導入

---
+++ spec/factories/players.rb
@@ -0,0 +1,76 @@
+FactoryBot.define do
+  factory :player do
+    association :team
+    sequence(:no) do |n|
+      n
+    end
+    name { 'テスト選手' }
+    position { Random.rand(1 .. 9) }
+    birthday { '1992/02/21' }
+    height { 178 }
+    weight { 78 }
+    throw { 1 }
+    hit { 2 }
+    detail { '詳細内容' }
+
+    trait :detail_of_nil do
+      detail { nil }
+    end
+
+    trait :no_of_nil do
+      no { nil }
+    end
+
+    trait :no_of_string do
+      no { '背番号' }
+    end
+
+    trait :name_of_nil do
+      name { nil }
+    end
+
+    trait :birthday_of_nil do
+      birthday { nil }
+    end
+
+    trait :position_of_nil do
+      position { nil }
+    end
+
+    trait :height_of_nil do
+      height { nil }
+    end
+
+    trait :height_of_string do
+      height { '身長' }
+    end
+
+    trait :height_of_a_digit do
+      height { 1 }
+    end
+
+    trait :height_of_four_digit do
+      height { 1234 }
+    end
+
+    trait :weight_of_string do
+      weight { '体重' }
+    end
+
+    trait :weight_of_a_digit do
+      weight { 1 }
+    end
+
+    trait :weight_of_four_digit do
+      weight { 1234 }
+    end
+
+    trait :throw_of_nil do
+      throw { nil }
+    end
+
+    trait :hit_of_nil do
+      hit { nil }
+    end
+  end
+end
--- spec/models/team_spec.rb
+++ spec/models/team_spec.rb
@@ -5,12 +5,7 @@ RSpec.describe Team, type: :model do
   #
   context '入力チェック正常系' do
     it "入力値が正しい時はバリデーションに引っかからないこと" do
-      team = Team.new(
-          name: '読売ジャイアンツ',
-          league: '1',
-          stadium: '東京ドーム',
-          address: '東京都渋谷区'
-      )
+      team = FactoryBot.build(:team)
       team.valid?
       expect(team).to be_valid
     end
@@ -18,45 +13,25 @@ RSpec.describe Team, type: :model do
 
   context '入力チェック異常系' do
     it '球団名(name)が未入力の時はバリデーションに引っかかること' do
-      team = Team.new(
-          name: nil,
-          league: '1',
-          stadium: '東京ドーム',
-          address: '東京都渋谷区'
-      )
+      team = FactoryBot.build(:team, :name_of_nil)
       team.valid?
       expect(team.errors.messages[:name]).to include('は必須です。')
     end
 
     it 'リーグ(league)が未入力の時はバリデーションに引っかかること' do
-      team = Team.new(
-          name: '読売ジャイアンツ',
-          league: nil,
-          stadium: '東京ドーム',
-          address: '東京都渋谷区'
-      )
+      team = FactoryBot.build(:team, :league_of_nil)
       team.valid?
       expect(team.errors.messages[:league]).to include('は必須です。')
     end
 
     it 'スタジアム(stadium)が未入力の時はバリデーションに引っかかること' do
-      team = Team.new(
-          name: '読売ジャイアンツ',
-          league: '1',
-          stadium: nil,
-          address: '東京都渋谷区'
-      )
+      team = FactoryBot.build(:team, :stadium_of_nil)
       team.valid?
       expect(team.errors.messages[:stadium]).to include('は必須です。')
     end
 
     it '住所(address)が未入力の時はバリデーションに引っかかること' do
-      team = Team.new(
-          name: '読売ジャイアンツ',
-          league: '1',
-          stadium: '東京ドーム',
-          address: nil
-      )
+      team = FactoryBot.build(:team, :address_of_nil)
       team.valid?
       expect(team.errors.messages[:address]).to include('は必須です。')
     end
