4dc11ba98d7fc5ae93a3386c4753ec2db4f7dc17
sohakahori <sohakahori@gmail.com>
2018-11-17 10:51:19 +0900

    Admin更新、削除処理追加

--- app/controllers/admin/admins_controller.rb
+++ app/controllers/admin/admins_controller.rb
@@ -1,6 +1,9 @@
 class Admin::AdminsController < ApplicationController
   layout 'admin'
 
+  before_action :authenticate_admin!
+  before_action :set_admin, only:[:edit, :update, :destroy]
+
   def index
     @admins = Admin.updated_at_desc.id_desc.page(params[:page]).per(15)
   end
@@ -20,6 +23,25 @@ class Admin::AdminsController < ApplicationController
     end
   end
 
+  def edit
+  end
+
+  def update
+    if @admin.update(admin_params)
+      flash[:success] = "管理者の変更に成功しました。"
+      redirect_to admin_admins_path
+    else
+      flash.now[:danger] = "入力項目に不備があります。"
+      render :edit
+    end
+  end
+
+  def destroy
+    @admin.destroy
+    flash[:success] = "管理者の削除に成功しました。"
+    redirect_to admin_admins_path
+  end
+
   private
   def admin_params
     params.require(:admin).permit(
@@ -30,5 +52,9 @@ class Admin::AdminsController < ApplicationController
         :password,
         :password_confirmation
     )
+  end
+
+  def set_admin
+    @admin = Admin.find(params[:id])
   end
 end
--- app/helpers/admin/admins_helper.rb
+++ app/helpers/admin/admins_helper.rb
@@ -1,2 +1,5 @@
 module Admin::AdminsHelper
+  def is_new_request?
+    request.path_info == new_admin_admin_path
+  end
 end
