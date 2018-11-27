3a6ef70e9744ab830bffea287682bf701dd9fcdc
sohakahori <uu138994@RLSUU138994M01.local>
2018-11-27 20:21:26 +0900

    API:response(レスポンス)作成処理追加

--- app/controllers/api/v1/responses_controller.rb
+++ app/controllers/api/v1/responses_controller.rb
@@ -1,6 +1,29 @@
 class Api::V1::ResponsesController < Api::V1::ApplicationController
+
+  before_action :get_board, only: [:index, :create, :destroy]
+
   def index
-    @responses = Board.find(params[:board_id]).responses.page(params[:page]).per(30)
+    @responses = @board.responses.page(params[:page]).per(30)
     render 'index', formats: 'json', handlers: 'jbuilder'
+  end
+
+  def create
+    @response = @board.responses.build(response_params)
+    @response.user = current_user
+    if @response.save
+      render 'create', formats: 'json', handlers: 'jbuilder' and return
+    else
+      @params = params
+      render 'error', formats: 'json', handlers: 'jbuilder' and return
+    end
+  end
+
+  private
+  def response_params
+    params.require(:response).permit(:body)
+  end
+
+  def get_board
+    @board = Board.find(params[:board_id])
   end
 end
--- app/models/response.rb
+++ app/models/response.rb
@@ -3,4 +3,6 @@ class Response < ApplicationRecord
   # アソシエーション
   belongs_to :user
   belongs_to :board
+
+  validates :body, presence: true
 end
