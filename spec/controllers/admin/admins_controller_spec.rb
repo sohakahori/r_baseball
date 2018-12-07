b02d5b587a07bdc9ef01fe2ee2407d1b77f5ef98
sohakahori <uu138994@RLSUU138994M01.local>
2018-12-07 17:35:35 +0900

    rspec追加(controllers/admion/admoin_controller)

--- spec/controllers/admin/admins_controller_spec.rb
+++ spec/controllers/admin/admins_controller_spec.rb
@@ -15,6 +15,11 @@ RSpec.describe Admin::AdminsController, type: :controller do
       get :index
       expect(response.status).to eq 302
     end
+
+    it 'csv形式でレスポンスされること' do
+      get :index, format: :csv
+      expect(response.content_type).to eq "text/csv"
+    end
   end
 
   describe '# new' do
